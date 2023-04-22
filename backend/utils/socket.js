cffd3ff5c4c57948c7f2d06ec7603643e30e0e8a
sainath47 <reddysainath47@gmail.com>
2023-04-22 05:46:40 +0530

    depl

--- backend/utils/socket.js
+++ backend/utils/socket.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 const {Server} = require('socket.io')
 const io = new Server(server ,{
   cors:{
-    origin: 'http://localhost:3000',
+    origin: 'https://sainath-socket.onrender.com',
     methods:['GET', "POST","PUT", "DELETE"],
   },
 });
